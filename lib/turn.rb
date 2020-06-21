c9a72158a90d5f674907eb665b09b9f9ece54f8f
Jzavier Timm <jztimm@gmail.com>
2020-06-21 17:35:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,7 @@
 # end
 
 
-#displays a tic tac toe board, with board spaces passed as an array
+# displays a tic tac toe board, with board spaces passed as an array
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -18,7 +18,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
+# makes user's move on the board to the index int
 def input_to_index(user_input)
   index = user_input.to_i
   index -= 1
